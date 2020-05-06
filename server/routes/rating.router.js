a2fc36a86b2c49b4793055d3aada87ffa133a65c
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-06 13:35:30 -0600

    Get routes in review, rating, series

--- server/routes/rating.router.js
+++ server/routes/rating.router.js
@@ -2,4 +2,23 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+
+//    GET all Ratings
+
+router.get("/", rejectUnauthenticated, (req, res) => {
+  const queryText = `SELECT * FROM "rating" ORDER BY "rating" ASC;`;
+  pool
+    .query(queryText)
+    .then((responseFromDb) => {
+      res.send(responseFromDb.rows);
+    })
+    .catch((error) => {
+      console.log("Get All Rating Error: ", error);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
