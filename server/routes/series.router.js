09cecd337850638a9d3f91355b31cb4789a6dbc8
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-06 13:25:24 -0600

    added get route for getting all series to homepage

--- server/routes/series.router.js
+++ server/routes/series.router.js
@@ -2,4 +2,23 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
+
+//    GET all Series  for home page
+
+router.get("/", rejectUnauthenticated, (req, res) => {
+  const queryText = `SELECT * FROM "series" ORDER BY "series" ASC;`;
+  pool
+    .query(queryText)
+    .then((responseFromDb) => {
+      res.send(responseFromDb.rows);
+    })
+    .catch((error) => {
+      console.log("Get All Series Error: ", error);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
