ce683c0edefcb4050ec5a16a3eff34b7c01733e7
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-13 14:42:01 -0600

    added routes to app.js from homepage

--- src/components/SeriesList.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-
-class SeriesList extends Component {
-  handleSelectGenre = (id) => (event) => {
-    this.props.history.push(`/genre/${id}`);
-  };
-}
-export default withRouter(SeriesList);
