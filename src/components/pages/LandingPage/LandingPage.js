ca9aee061caf81ef4c71aa9cbede79a9cc470db6
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-13 10:04:40 -0600

    fixed issues with landind page

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -13,24 +13,13 @@ class LandingPage extends Component {
     this.props.history.push("/login");
   };
 
-  //   use component did mount to dispatch an action to request the LandingPage from the API
-  componentDidMount() {
-    this.props.dispatch({ type: "GET_SERIES" });
-  }
-
   render() {
     return (
       <div className="container">
         <h2>{this.state.heading}</h2>
 
         <div className="grid">
-          <div className="grid-col grid-col_8">
-            <p></p>
-
-            <p></p>
-
-            <p></p>
-          </div>
+          <div className="grid-col grid-col_8"></div>
           <div className="grid-col grid-col_4">
             <h3>Already a Member?</h3>
             <button className="btn btn_sizeFull" onClick={this.onLogin}>
