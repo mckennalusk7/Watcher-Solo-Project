0b7d1834c12b1bad8048b9350de6f0e63e1a4b2b
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-13 10:45:30 -0600

    created series list to render to the home page for the user

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,4 +2,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 
+import "./HomePage.css";
+
+class HomePage extends Component {
+  componentDidMount() {
+    this.props.dispatch({ type: "GET_SERIES" });
+  }
+  render() {
+    return (
+      <div>
+        {this.props.store.seriesReducer.map((item, index) => {
+          return <seriesList key={index} series={item} />;
+        })}
+      </div>
+    );
+  }
+}
+
 export default connect(mapStoreToProps)(HomePage);
---
+++ src/components/SeriesList/SeriesList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+// import Grid from "@material-ui/core/Grid";
+
+import "./MovieList.css";
+
+class SeriesList extends Component {
+  };
+
+  render() {
+    return (
+    
+    );
+  }
+}
+
+export default withRouter(SeriesList);
