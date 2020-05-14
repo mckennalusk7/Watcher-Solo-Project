88436ccdaba6532a1d92f8a1ef9c9e9b8077c013
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-14 09:15:22 -0600

    created buttons on nav bar for user to be directed to different pages

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -31,6 +31,14 @@ const Nav = (props) => {
         {/* Show the link to the info page and the logout button if the user is logged in */}
         {props.store.user.id && (
           <>
+            <Link className="nav-link" to="/series">
+              {" "}
+              My Series
+            </Link>
+            <Link className="nav-link" to="/genres">
+              {" "}
+              Search
+            </Link>
             <Link className="nav-link" to="/info">
               Info Page
             </Link>
