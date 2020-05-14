13f3505517fb0e8244818dac933eef5ada198027
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-14 13:00:43 -0600

    adding file so user may upload a profile picture

--- src/components/pages/UserPage/UserPage.js
+++ src/components/pages/UserPage/UserPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import LogOutButton from "../../LogOutButton/LogOutButton";
@@ -7,16 +7,28 @@ import mapStoreToProps from "../../../redux/mapStoreToProps";
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
-  <div>
-    <div>
-      <h1 id="welcome"> Hello, {props.store.user.username}!</h1>{" "}
-      <button id="follow-button"> Follow</button>
-    </div>
+class UserPage extends Component {
+  fileSelectHandler = (event) => {
+    console.log(event.target.files[0]);
+  };
 
-    <LogOutButton className="log-in" />
-  </div>
-);
+  render() {
+    return (
+      <div>
+        <div>
+          <h1 id="welcome"> Hello, {this.props.store.user.username}!</h1>{" "}
+          <button id="follow-button"> Follow</button>
+        </div>
+        <div className="add_photo">
+          <input type="file" onChange={this.fileSelectHandler} />
+          <button onClick={this.fileUploadHandler}>Upload</button>
+        </div>
+
+        <LogOutButton className="log-in" />
+      </div>
+    );
+  }
+}
 
 // this allows us to use <App /> in index.js
 export default connect(mapStoreToProps)(UserPage);
