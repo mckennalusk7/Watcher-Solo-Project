b7b6ee2404e7fe0410734d234479f8ed695ecb3c
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-14 09:55:31 -0600

    updates to the about page

--- src/components/pages/AboutPage/AboutPage.js
+++ src/components/pages/AboutPage/AboutPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -9,7 +9,12 @@ const AboutPage = () => (
   <div>
     <div>
       <p>
-        This about page is for anyone to read!
+        Watcher is an application that allows users to research, explore, and
+        discuss their favorite TV Series (recent and classics) with other users.
+        By registering to the app, you will be able to search and review
+        specific genres to meet their interests. This access allows you to
+        comment and discuss your favorite series and episodes. A social app for
+        people to connect through discussing TV Series.
       </p>
     </div>
   </div>
