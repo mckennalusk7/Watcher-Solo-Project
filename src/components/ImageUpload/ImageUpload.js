3a302a8b4b38454c2c24030f3b13ef3d059064ef
McKenna Lusk <mckenna.lusk7@gmail.com>
2020-05-15 13:40:35 -0600

    dropzone components in imageupload

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-class ImageUpload extends Component {}
+class ImageUpload extends Component {
+  render() {
+    const uploadOptions = {
+      server: "http://localhost:4000",
+      signingUrlQueryParams: { uploadType: "avatar" },
+    };
+    const s3Url = "https://my-bucket.s3.amazonaws.com";
+
+    return (
+      <DropzoneS3Uploader
+        onFinish={this.handleFinishedUpload}
+        s3Url={s3Url}
+        maxSize={1024 * 1024 * 5}
+        upload={uploadOptions}
+      />
+    );
+  }
+}
 
 export default connect()(ImageUpload);
